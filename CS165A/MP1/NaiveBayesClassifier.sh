de33d009acd9b000b99159c97eba8ae024a495c9
Evania Cheng <evania@ucsb.edu>
2024-11-11 02:52:51 -0800

    MP1 new files

--- CS165A/MP1/NaiveBayesClassifier.sh
+++ CS165A/MP1/NaiveBayesClassifier.sh
@@ -1,6 +1,6 @@
 # Paths to traing and validation paths
-TRAIN_DATA_PATH="path/to/train_data.csv"
-VALIDATE_DATA_PATH="path/to/validate_data.csv"
+TRAIN_DATA_PATH="/Users/evaniacheng/Desktop/evaniacheng/CS165A/MP1/train_data.csv"
+VALIDATE_DATA_PATH="/Users/evaniacheng/Desktop/evaniacheng/CS165A/MP1/validation_data.csv"
 
 # Run the Python script with paths to the data files
 python3 NaiveBayesClassifier.py "$TRAIN_DATA_PATH" "$VALIDATE_DATA_PATH"
